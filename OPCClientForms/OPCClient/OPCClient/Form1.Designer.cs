0bf2e6609c439a1503479d2bcdb4dd6698f8f83e
valex <avarfolo@gmail.com>
2016-07-26 03:53:39 -0700

    commit

---
+++ OPCClientForms/OPCClient/OPCClient/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace OPCClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.Element = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Quality = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(188, 265);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(194, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(160, 265);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Element,
+            this.Value,
+            this.Quality});
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "listViewGroup1";
+            this.listView2.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1});
+            this.listView2.Location = new System.Drawing.Point(0, 271);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(545, 186);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // Element
+            // 
+            this.Element.Text = "Element";
+            this.Element.Width = 180;
+            // 
+            // Value
+            // 
+            this.Value.Text = "Value";
+            this.Value.Width = 180;
+            // 
+            // Quality
+            // 
+            this.Quality.Text = "Quality";
+            this.Quality.Width = 180;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 537);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader Element;
+        private System.Windows.Forms.ColumnHeader Value;
+        private System.Windows.Forms.ColumnHeader Quality;
+    }
+}
+
---
+++ OPCClientForms/OPCClient/OPCClient/Form1.cs
@@ -0,0 +1,212 @@
+﻿using Opc;
+using Opc.Da;
+using OpcCom;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OPCClient
+{
+    public partial class Form1 : Form
+    {
+
+        Opc.Da.Server server = null;
+        public Form1()
+        {
+            InitializeComponent();
+            OpcCom.ServerEnumerator se = new ServerEnumerator();
+            Opc.Server[] Servers;
+            String sErrFunc = "";
+
+            Opc.ConnectData cd = new Opc.ConnectData(new System.Net.NetworkCredential());
+            sErrFunc = "GetAvailableServers";
+            Servers = se.GetAvailableServers(Opc.Specification.COM_DA_20, "localhost", cd);
+            Console.WriteLine(Servers[1].Name.ToString());
+
+
+            Opc.URL url = new Opc.URL("opcda://localhost/" + Servers[0].Name);
+            
+            OpcCom.Factory fact = new OpcCom.Factory();
+            server = new Opc.Da.Server(fact, null);
+            server.Connect(url, new Opc.ConnectData(new System.Net.NetworkCredential()));
+        }
+
+        private void getTree(Opc.Da.Server server)
+        {
+            BrowsePosition bp;
+            ItemIdentifier itemID = new ItemIdentifier();
+            BrowseFilters filters = new BrowseFilters();
+            BrowseElement[] be = server.Browse(itemID, filters, out bp);
+            TreeNode kepServ = null;
+            int i = be.Length;
+          //  MessageBox.Show(i.ToString());
+            while (i != 0)
+            {
+                --i;
+                kepServ = treeView1.Nodes.Add(be[i].ItemName);
+            }
+          //  treeView1.Nodes.Add(kepServ);
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getTree(server);
+        }
+
+        private Object getOPCValue(String ItemName, BrowseElement[] be)
+        {
+            // Create a group
+            Opc.Da.Subscription group;
+            Opc.Da.SubscriptionState groupState = new Opc.Da.SubscriptionState();
+            groupState.Name = ItemName;
+            groupState.Active = true;
+            group = (Opc.Da.Subscription)server.CreateSubscription(groupState);
+            //Console.WriteLine(group.);
+
+
+            // add items to the group.
+            Opc.Da.Item[] items = new Opc.Da.Item[1];
+
+                items[0] = new Opc.Da.Item();
+                items[0].ItemName = ItemName;
+
+            items = group.AddItems(items);
+
+            ItemValueResult[] ir = group.Read(items);
+            return ir[0].Value;
+        }
+
+
+
+        private void DisplayChildren(TreeNode ParentNode, TreeNodeMouseClickEventArgs e)
+        {
+          
+            BrowsePosition bp;
+            ItemIdentifier itemID = new ItemIdentifier(e.Node.FullPath.Replace("\\","."), e.Node.FullPath.Replace("\\","."));
+            BrowseFilters filters = new BrowseFilters();
+
+            BrowseElement[] be = server.Browse(itemID, filters, out bp);
+           // MessageBox.Show(bp.MaxElementsReturned.ToString());
+
+            if (be != null)
+            {
+                
+                int i = be.Length;
+                // Вначале выводим все листья на данном уровне
+                while (i != 0)
+                {
+                    --i;
+                    TreeNode tvNode;
+
+                    if (ParentNode == null)
+                    { 
+                        tvNode = treeView1.Nodes.Add(be[i].Name);
+                        if (be[i].IsItem)
+                        {
+                            listView1.Items.Add(be[i].Name);
+                            Object irr = getOPCValue(e.Node.FullPath.Replace("\\", ".") + "." + be[i].Name, be);
+
+                        }
+
+                    }
+                    else
+                    {
+                        if (be[i].IsItem)
+                        {
+                            ListViewItem lvItem = new ListViewItem();
+                            ListViewItem.ListViewSubItem[] lvSubItem = new ListViewItem.ListViewSubItem[3];
+
+                            Object irr = getOPCValue(e.Node.FullPath.Replace("\\", ".") + "." + be[i].Name, be);
+
+                            lvItem.Text = e.Node.FullPath.Replace("\\", ".") + "." + be[i].Name;
+                            //lvItem.SubItems.Add(getOPCValue(be[i].ItemName).ToString());
+                            lvItem.SubItems.Add(irr.ToString());
+
+                            listView2.Items.Add(lvItem);
+                        }
+                        else {
+                            tvNode = ParentNode.Nodes.Add(be[i].Name);
+                        }
+                     
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("be null");
+            }
+
+
+           //// BrowsePosition bp2;
+           //// ItemIdentifier itemID2 = new ItemIdentifier(e.Node.NextNode.Text, e.Node.NextNode.Text);
+           //// BrowseFilters filters2 = new BrowseFilters();
+           //// BrowseElement[] be2 = server.Browse(itemID, filters, out bp);
+        //    while (cnt != 0)
+        //    {
+        //        TreeNode tvNode;
+        //        if (ParentNode == null)
+        //            tvNode = treeView1.Nodes.Add(be[i].ItemName);
+        //        else
+        //            tvNode = ParentNode.Nodes.Add(strName);
+        //       // pParent.ChangeBrowsePosition(tagOPCHDA_BROWSEDIRECTION.OPCHDA_BROWSE_DOWN, strName);
+        //        DisplayChildren(tvNode, pParent); //Рекурсивно вызываем метод
+        //        //После того как все дочерние узлы показаны постепенно поднимаемся
+        //        //вверх
+        //        pParent.ChangeBrowsePosition(tagOPCHDA_BROWSEDIRECTION.OPCHDA_BROWSE_UP,
+        //        strName);
+        //        pEnum.RemoteNext(1, out strName, out cnt);
+        //    }
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            DisplayChildren(e.Node,e);
+             
+          //  MessageBox.Show(e.Node.FullPath);
+          //  BrowsePosition bp;
+          //  ItemIdentifier itemID = new ItemIdentifier(e.Node.Text, e.Node.Text);
+          //  BrowseFilters filters = new BrowseFilters();
+          //  BrowseElement[] be = server.Browse(itemID, filters, out bp);
+
+          //  TreeNode kepServ = null;
+          //  //MessageBox.Show( treeView1.GetNodeCount(true).ToString());
+            
+          //  int i = be.Length;
+          ////  MessageBox.Show(be.Length.ToString());
+          //  while (i != 0)
+          //  {
+          //      if (be[i-1].HasChildren)
+          //      {
+          //          kepServ = new TreeNode(be[i - 1].ItemName);
+
+          //          treeView1.Nodes[treeView1.GetNodeCount(false) - 1].Nodes.Add(kepServ);
+                    
+          //      }
+
+          //      else
+          //      {
+          //        //  MessageBox.Show("The tree is finishe there");
+          //      }
+
+          
+               
+          //      i--;
+          //  }
+
+
+
+           // e.Node.TreeView
+
+        }
+
+
+    }
+}
